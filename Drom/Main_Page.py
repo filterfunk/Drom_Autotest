ca1f77621b409383c5c1b9c1b9d6500687311e24
filterfunk <dbst90@gmail.com>
2020-09-16 18:00:38 +0700

    Drom Autotest 16 Сентября

---
+++ Drom/Main_Page.py
@@ -0,0 +1,47 @@
+# Набор методов на главной странице
+from selenium import webdriver
+import time
+import pytest
+
+link = "https://www.drom.ru"
+
+
+@pytest.fixture
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    # этот код выполнится после завершения теста
+    print("\nquit browser..")
+    browser.quit()
+
+
+class LargusSearch:
+    @pytest.mark.smoke
+    def test_we_can_find_largus(self, browser):
+        print("test_we_can_find_largus")
+        browser.get(link)
+        # Открыть Продажа авто в России
+        browser.find_element_by_xpath("//h2[@class='css-1cmbqcv e18vbajn0']").click()
+        # Радиус поиска +1000км
+        browser.find_element_by_xpath("//a[@class='css-ifsycf e1wvjnck0'][2]").click()
+        # Прокручиваем вниз, чтобы было видно списки моделей
+        element = browser.find_element_by_xpath("//button[contains(text(), 'Марка')]")
+        browser.execute_script("return arguments[0].scrollIntoView(true);", element)
+        # Найти Ларгусы
+        brand = browser.find_element_by_xpath("//button[contains(text(), 'Марка')]").click()
+        brand1 = browser.find_element_by_xpath("//div[contains(text(), 'Лада')][2]").click()
+        model = browser.find_element_by_xpath("//button[contains(text(), 'Модель')][1]").click()
+        model1 = browser.find_element_by_xpath("//div[contains(text(), 'Ларгус')][1]").click()
+        year = browser.find_element_by_xpath("//button[contains(text(), 'Год от')]").click()
+        year1 = browser.find_element_by_xpath("//div[contains(text(), '2015')][1]").click()
+        # Прокручиваем вниз до кнопки Отправить и нажимаем её
+        submit = browser.find_element_by_xpath("//button[@type='submit']")
+        browser.execute_script("return arguments[0].scrollIntoView(true);", submit)
+        submit = browser.find_element_by_xpath("//button[@type='submit']").click()
+        userpage = browser.current_url
+        print(userpage)
+        assert userpage == "https://novosibirsk.drom.ru/lada/largus/?minyear=2015&distance=1000"
+        time.sleep(5)
+        browser.quit()
+
